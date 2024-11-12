c673da34606caffbf54f2480e5dfb331777c9e01
holas <jgomez.24@est.ucab.edu.ve>
2024-11-11 21:14:26 -0400

    se crea la clase Tablero

---
+++ src/Tablero.java
@@ -0,0 +1,17 @@
+public class Tablero {
+    private static final int TAMANO = 15;
+    private char[][] tablero;
+
+    public Tablero() {
+        tablero = new char[TAMANO][TAMANO];
+        inicializarTablero();
+    }
+
+    private void inicializarTablero() {
+        for (int i = 0; i < TAMANO; i++) {
+            for (int j = 0; j < TAMANO; j++) {
+                tablero[i][j] = ' ';
+            }
+        }
+    }
+}
