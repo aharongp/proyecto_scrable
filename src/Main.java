34d9bc4a933a0ed18af68e1775c8598ffcbdf50d
PanzerOg <jose.a.montesdeoca28@gmail.com>
2024-11-29 13:14:03 -0400

    *Se modifico el main para pruebas

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,13 @@
 import java.util.Scanner;
 
 public class Main {
-    public static Scanner read = new Scanner(System.in);
-    public static void main(String[] args) {
-        SpanishBag bag = new SpanishBag();
-        Tablero tablero = new Tablero(bag);
-        Jugador jugador = new Jugador("Player1", "email@example.com");
 
-// Dar fichas iniciales al jugador
-        jugador.addCharacters(bag.get(7));
-        jugador.printCharacters();
-        String palabra = read.next();
-// Intentar colocar una palabra
-        boolean resultado = tablero.colocarPalabra(palabra, 7, 7, true, jugador);
-        tablero.mostrarTablero();
-        jugador.printCharacters();
+    public static Scanner read = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Jugador jugador1 = new Jugador("Pepe", "pepe@gmail.com");
+        Jugador jugador2 = new Jugador("Paco", "paco@gmail.com");
+        Partida partida = new Partida(jugador1, jugador2);
+        partida.game();
     }
 }
