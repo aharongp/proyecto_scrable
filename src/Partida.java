7560a42b9c79d7bfa8db57354d82cf83a3b5fe6f
PanzerOg <jose.a.montesdeoca28@gmail.com>
2024-11-29 13:40:01 -0400

    *Se puso private el startgame *Se creo el metodo turnoJugador por mucho codigo en metodo game *Se modifico metodo game *Se agrego metodo reponer fichas

--- src/Partida.java
+++ src/Partida.java
@@ -1,8 +1,8 @@
-import javax.management.openmbean.ArrayType;
-import java.lang.reflect.Array;
-import java.util.Random;
+
+import java.util.ArrayList;
 
 public class Partida {
+
     private Jugador jugador1;
     private Jugador jugador2;
     private int score1;
@@ -15,7 +15,6 @@ public class Partida {
     private long time;
     private int winner;
 
-
     public Partida(Jugador jugador1, Jugador jugador2) {
         this.jugador1 = jugador1;
         this.jugador2 = jugador2;
@@ -27,69 +26,85 @@ public class Partida {
         this.winner = 0;
     }
 
-    public void alternarTurno(){
-        if (this.actualTurn == 1){ this.actualTurn= 2; }
-        else{ this.actualTurn = 1; }
+    public void alternarTurno() {
+        if (actualTurn == 1) {
+            actualTurn = 2;
+        }
+        else {
+            actualTurn = 1;
+        }
     }
 
-    public void startGame(){
-        this.jugador1.addCharacters(bag.get(7));
-        this.jugador2.addCharacters(bag.get(7));
-        this.initialTime = System.currentTimeMillis();
+    private void startGame() {
+        jugador1.addCharacters(bag.get(7));
+        jugador2.addCharacters(bag.get(7));
+        initialTime = System.currentTimeMillis();
     }
 
-    public void game(){
-        startGame();
-        while (!chooseWinner()){
-            this.tablero.mostrarTablero();
-            if(this.actualTurn == 1){
-                System.out.println("Es turno del jugador 1: " + jugador1.getAlias());
-                jugador1.printCharacters();
-                System.out.println("Escribe la palabra que quieres poner");
-                String word = Main.read.next();
-                if (jugador1.validarCaracteres(word)){
-                    System.out.println("¿En que posicion la deseas colocar?\s numero de fila: ");
-                    int fila = Main.read.nextInt();
-                    System.out.println("numero de columna: ");
-                    int columna = Main.read.nextInt();
-                    System.out.println("""
+    public void turnoJugador(Jugador jugador) {
+        System.out.println("Es turno del jugador " + jugador.getAlias());
+        jugador.printCharacters();
+        System.out.println("Escribe la palabra que quieres poner");
+        String word = ScreenReader.read.next();
+        if (jugador.validarCaracteres(word)) {
+            System.out.println("¿En que posicion la deseas colocar?\s numero de fila: ");
+            int fila = ScreenReader.read.nextInt();
+            System.out.println("numero de columna: ");
+            int columna = ScreenReader.read.nextInt();
+            System.out.println("""
                             Escribe la direccion de la palabra:\s
                              1. Horizontal\s
                              2. Vertical""");
-                    int direccion = Main.read.nextInt();
-                    if (direccion == 1) {
-                        this.tablero.colocarPalabra(word, fila,columna,true, jugador1);
-                        alternarTurno();
-                    } else if (direccion == 2) {
-                        this.tablero.colocarPalabra(word, fila,columna,false, jugador1);
-                        alternarTurno();
-                    }
-
-                }
-            }else{
-                jugador2.printCharacters();
+            int direccion = ScreenReader.read.nextInt();
+            if (direccion == 1) {
+                this.tablero.colocarPalabra(word, fila, columna, true, jugador);
             }
+            else if (direccion == 2) {
+                this.tablero.colocarPalabra(word, fila, columna, false, jugador);
+            }
+            reponerFichas(jugador);
+        }
+    }
+
+    public void reponerFichas(Jugador jugador) {
+        int fichasNecesarias = 7 - jugador.getNumberOfCharacters();
+        if (fichasNecesarias > 0) {
+            ArrayList<Character> nuevasFichas = bag.get(fichasNecesarias);
+            jugador.addCharacters(nuevasFichas);
+        }
+    }
+
+    public void game() {
+        startGame();
+        while (!chooseWinner()) {
+            this.tablero.mostrarTablero();
+            if (actualTurn == 1) {
+                turnoJugador(jugador1);
+            }
+            else {
+                turnoJugador(jugador2);
+            }
+            alternarTurno();
         }
         finishGame();
     }
 
-    public void finishGame(){
+    public void finishGame() {
         this.finishtime = System.currentTimeMillis();
         this.time = this.finishtime - this.initialTime;
-
     }
 
-    public boolean chooseWinner(){
-        if (this.bag.getNumberOfCharacters() == 0){
-            if(this.jugador1.getNumberOfCharacters() == 0){
+    public boolean chooseWinner() {
+        if (this.bag.remaning() == 0) {
+            if (this.jugador1.getNumberOfCharacters() == 0) {
                 this.winner = 1;
                 return true;
-            } else if (this.jugador2.getNumberOfCharacters() == 0) {
+            }
+            else if (this.jugador2.getNumberOfCharacters() == 0) {
                 this.winner = 2;
                 return true;
             }
         }
         return false;
     }
-
 }
