9b7f038e52d020ef04220c46c576da693c593abd
PanzerOg <jose.a.montesdeoca28@gmail.com>
2024-11-29 13:34:08 -0400

    *Se cambio el get de number para que no tenga conflictos con jackson json

--- src/SpanishBag.java
+++ src/SpanishBag.java
@@ -1,12 +1,14 @@
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
-public class SpanishBag implements CharactersBag{
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class SpanishBag implements CharactersBag {
     private ArrayList<Character> characterList;
 
     public SpanishBag() {
-        characterList=new ArrayList<>();
+        characterList = new ArrayList<>();
         addAll();
     }
 
@@ -14,7 +16,7 @@ public class SpanishBag implements CharactersBag{
         return characterList;
     }
 
-    public int getNumberOfCharacters(){
+    public int numberOfCharacters() {
         return characterList.size();
     }
 
@@ -24,14 +26,14 @@ public class SpanishBag implements CharactersBag{
 
     @Override
     public ArrayList<Character> get(int n) {
-        ArrayList<Character> extractedCharacters=new ArrayList<>();
-        if(characterList.size()>=n){
-            for(int i=0;i!=n;i++){
+        ArrayList<Character> extractedCharacters = new ArrayList<>();
+        if (characterList.size() >= n) {
+            for (int i = 0; i != n; i++) {
                 extractCharacters(extractedCharacters);
             }
         }
         else {
-            if(characterList.size()<n) {
+            if (characterList.size() < n) {
                 while (!characterList.isEmpty()) {
                     extractCharacters(extractedCharacters);
                 }
@@ -46,58 +48,58 @@ public class SpanishBag implements CharactersBag{
         addAll();
     }
 
-    private void extractCharacters(ArrayList<Character> list){
-        Random random=new Random();
-        int randomIndex= random.nextInt(characterList.size());
+    private void extractCharacters(ArrayList<Character> list) {
+        Random random = new Random();
+        int randomIndex = random.nextInt(characterList.size());
         Character randomItem = characterList.get(randomIndex);
         list.add(randomItem);
         characterList.remove(randomItem);
     }
 
-    private void add(String symbol,int point,int times){
-        for(int i=0;i!=times;i++){
-            Character character=new Character(symbol,point);
+    private void add(String symbol, int point, int times) {
+        for (int i = 0; i<times; i++) {
+            Character character = new Character(symbol, point);
             characterList.add(character);
         }
     }
 
-    private void addAll(){
-        add("A",1,12);
-        add("B",4,2);
-        add("C",3,4);
-        add("CH",8,1);
-        add("D",3,5);
-        add("E",1,12);
-        add("F",5,1);
-        add("G",3,2);
-        add("H",5,2);
-        add("I",1,6);
-        add("J",10,1);
-        add("L",2,4);
-        add("LL",8,1);
-        add("M",3,2);
-        add("N",2,5);
-        add("Ñ",10,1);
-        add("O",1,9);
-        add("P",4,2);
-        add("Q",8,1);
-        add("R",2,5);
-        add("RR",8,1);
-        add("S",1,6);
-        add("T",1,4);
-        add("U",1,5);
-        add("V",4,1);
-        add("X",10,1);
-        add("Y",5,1);
-        add("Z",10,1);
-        add("Comodin",0,2);
+    private void addAll() {
+        add("A", 1, 12);
+        add("B", 4, 2);
+        add("C", 3, 4);
+        add("CH", 8, 1);
+        add("D", 3, 5);
+        add("E", 1, 12);
+        add("F", 5, 1);
+        add("G", 3, 2);
+        add("H", 5, 2);
+        add("I", 1, 6);
+        add("J", 10, 1);
+        add("L", 2, 4);
+        add("LL", 8, 1);
+        add("M", 3, 2);
+        add("N", 2, 5);
+        add("Ñ", 10, 1);
+        add("O", 1, 9);
+        add("P", 4, 2);
+        add("Q", 8, 1);
+        add("R", 2, 5);
+        add("RR", 8, 1);
+        add("S", 1, 6);
+        add("T", 1, 4);
+        add("U", 1, 5);
+        add("V", 4, 1);
+        add("X", 10, 1);
+        add("Y", 5, 1);
+        add("Z", 10, 1);
+        add("Comodin", 0, 2);
     }
 
     @Override
     public String toString() {
-        return "SpanishBag{" +
-                "characterList=" + characterList +
-                '}';
+        return "SpanishBag{"
+                + "characterList=" + characterList
+                + '}';
     }
 
     @Override
