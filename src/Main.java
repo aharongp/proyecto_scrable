3d3d1533a19211b54421670d1870283cc817ebc6
PanzerOg <jose.a.montesdeoca28@gmail.com>
2024-11-30 20:05:59 -0400

    *Se modifico el main para guardar la partida y luego se probo el cargar partida comentado la inicializacion de la partida

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,17 @@ public class Main {
     public static Scanner read = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Jugador jugador1 = new Jugador("Pepe", "pepe@gmail.com");
-        Jugador jugador2 = new Jugador("Paco", "paco@gmail.com");
-        Partida partida = new Partida(jugador1, jugador2);
-        partida.game();
+        //Jugador jugador1 = new Jugador("Pepe", "pepe@gmail.com");
+        //Jugador jugador2 = new Jugador("Paco", "paco@gmail.com");
+        //Partida partida = new Partida(jugador1, jugador2);
+        //partida.game();
+        recuperar();
+    }
+
+    public static void recuperar(){
+        ManejadorDeArchivos manejadorDeArchivos=new ManejadorDeArchivos();
+        String nombre="84fcc350-f4a0-4487-a749-1dacab9b5e34";
+        Partida partida=manejadorDeArchivos.restaurar(nombre);
+        partida.continuarPartida();
     }
 }
