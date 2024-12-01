486415e3d3ee6bc613a3a33107b6d74afde800fa
PanzerOg <jose.a.montesdeoca28@gmail.com>
2024-11-30 23:31:29 -0400

    *Se creo la clase Prueba para probar la clase estadistica

---
+++ src/Prueba.java
@@ -0,0 +1,18 @@
+public class Prueba {
+    public static void main(String[] args) {
+        test01();
+    }
+
+    public static void test00(){
+        Jugador jugador1 = new Jugador("Pepe", "pepe@gmail.com");
+        Jugador jugador2 = new Jugador("Paco", "paco@gmail.com");
+        Partida partida = new Partida(jugador1, jugador2);
+        partida.game();
+    }
+
+    public static void test01(){
+        Estadisticas estadisticas=new Estadisticas();
+        estadisticas.estad("Pepe");
+    }
+
+}
