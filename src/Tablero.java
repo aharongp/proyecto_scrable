bb11536139030fd412c34b74db4c6ab00706747d
josegg29 <jose.gregorio.jr.29@gmail.com>
2024-12-01 16:31:56 -0400

    En la clase Tablero se cambio la estructura de un arreglo de Character a un arreglo de Casilla para porder trabajar con casillas que almacenen tanto fichas como bonificaciones

--- src/Tablero.java
+++ src/Tablero.java
@@ -3,18 +3,45 @@ import java.util.ArrayList;
 public class Tablero {
     private static final int TAMANO = 15;
     private static final int MAX_FICHAS = 7;
-    private Character[][] tablero;
+    private Casilla[][] tablero;
+
 
     public Tablero() {
-        tablero = new Character[TAMANO][TAMANO];
+        tablero = new Casilla[TAMANO][TAMANO];
         inicializarTablero();
     }
 
     private void inicializarTablero() {
         for (int i = 0; i < TAMANO; i++) {
             for (int j = 0; j < TAMANO; j++) {
-                tablero[i][j] = null;
+                tablero[i][j] = new Casilla();
             }
+        }
+        String[] triplePalabra = {"0,0", "0,7", "0,14", "7,0", "7,14", "14,0", "14,7", "14,14"};
+        String[] doblePalabra = {"1,1", "2,2", "3,3", "4,4", "7,7", "10,10", "11,11", "12,12", "13,13"};
+        String[] tripleLetra = {"1,5", "5,1", "5,5", "5,9", "5,13", "9,1", "9,5", "9,9", "9,13", "13,5"};
+        String[] dobleLetra = {"0,3", "0,11", "2,6", "2,8", "3,0", "3,7", "3,14", "6,2", "6,6", "6,8", "6,12",
+                "7,3", "7,11", "8,2", "8,6", "8,8", "8,12", "11,0", "11,7", "11,14", "12,6", "12,8",
+                "14,3", "14,11"};
+
+        for (String pos : triplePalabra) {
+            String[] coords = pos.split(",");
+            tablero[Integer.parseInt(coords[0])][Integer.parseInt(coords[1])].setBonificacion("3W");
+        }
+
+        for (String pos : doblePalabra) {
+            String[] coords = pos.split(",");
+            tablero[Integer.parseInt(coords[0])][Integer.parseInt(coords[1])].setBonificacion("2W");
+        }
+
+        for (String pos : tripleLetra) {
+            String[] coords = pos.split(",");
+            tablero[Integer.parseInt(coords[0])][Integer.parseInt(coords[1])].setBonificacion("3L");
+        }
+
+        for (String pos : dobleLetra) {
+            String[] coords = pos.split(",");
+            tablero[Integer.parseInt(coords[0])][Integer.parseInt(coords[1])].setBonificacion("2L");
         }
     }
 
