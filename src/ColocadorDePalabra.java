667f783e074d9048b342b9a48a0a874064415e8f
PanzerOg <jose.a.montesdeoca28@gmail.com>
2024-12-01 22:49:44 -0400

    *Se refinaron varios metodos para que la palabra quedar correctamento colocada

--- src/ColocadorDePalabra.java
+++ src/ColocadorDePalabra.java
@@ -3,13 +3,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ColocadorDePalabra {
-    private int puntosGanados=0;
-    public boolean colocarPalabra(String palabra, int fila, int columna, boolean horizontal, Jugador jugador, Character[][] tablero) {
-        ArrayList<Character> fichasUsadas=new ArrayList<>();
-        boolean canPlace = canPlaceWord(palabra,fila,columna,jugador.getPlayerCharacters(),horizontal,tablero);
+    private int puntosGanados = 0;
+    private boolean enlazado=false;
+
+    public boolean colocarPalabra(String palabra, int fila, int columna, boolean horizontal, Jugador jugador, Character[][] tablero,boolean tableroVacio) {
+        ArrayList<Character> fichasUsadas = new ArrayList<>();
+        boolean canPlace = canPlaceWord(palabra, fila, columna, jugador.getPlayerCharacters(), horizontal, tablero,tableroVacio);
         if (canPlace) {
             System.out.println("The palabra \"" + palabra + "\" can be placed on the board.");
-            actualizarTablero(palabra, fila, columna, horizontal,tablero,jugador.getPlayerCharacters(),fichasUsadas);
+            actualizarTablero(palabra, fila, columna, horizontal, tablero, jugador.getPlayerCharacters(), fichasUsadas);
             jugador.getPlayerCharacters().getFichas().removeAll(fichasUsadas);
             jugador.addPoints(puntosGanados);
             jugador.addWordPlayed();
@@ -19,7 +21,9 @@ public class ColocadorDePalabra {
         return canPlace;
     }
 
-    public boolean canPlaceWord(String word,int row, int col,FichasJugador fichasJugador, boolean horizontal, Character[][] board) {
+    public boolean canPlaceWord(String word, int row, int col, FichasJugador fichasJugador, boolean horizontal, Character[][] board,boolean tableroVacio) {
+        int crow,ccol;
+        boolean enlazado=false;
         Map<String, Integer> tileCount = new HashMap<>();
         for (Character tile : fichasJugador.getFichas()) {
             tileCount.put(tile.getSymbol(), tileCount.getOrDefault(tile.getSymbol(), 0) + 1);
@@ -45,37 +49,49 @@ public class ColocadorDePalabra {
                 i++;
             }
             if (horizontal) {
-                col = col + (i - requiredTilesCount) - 1;
+                //ccol = col + (i - requiredTilesCount) - 1;
+                ccol = col + requiredTilesCount -1;
+                crow = row;
+            } else {
+                //crow = row + (i - requiredTilesCount) - 1;
+                crow = row + requiredTilesCount -1;
+                ccol = col;
             }
-            else {
-                row = row + (i - requiredTilesCount) - 1 ;
-            }
-            if (board[row][col]!=null && !board[row][col].getSymbol().equals(tile)) {
-                System.out.println("Letra en tablero no coincide : " + row +", "+col+","+"ficha=" +tile + ",tab="+ board[row][col].getSymbol());
+            System.out.println("Coords="+crow+","+ccol);
+            if (board[crow][ccol] != null && !board[crow][ccol].getSymbol().equals(tile)) {
+                System.out.println("Letra en tablero no coincide : " + crow + ", " + ccol + "," + "ficha=" + tile + ",tab=" + board[crow][ccol].getSymbol());
                 return false;
             }
             else {
-               System.out.println("Pasa en tablero");
+                System.out.println("Pasa en tablero");
             }
-            if (board[row][col]!=null && tileCount.getOrDefault(tile, 0) <= 0) {
-                System.out.println("Jugador no tiene letra : " + row + ", " + col + "," + "ficha=" + tile + ",tab=" + board[row][col].getSymbol());
+            if (board[crow][ccol] == null && (tileCount.getOrDefault(tile, 0) <= 0)) {
+                System.out.println("Jugador no tiene letra : " + crow + ", " + ccol + "," + "ficha=" + tile);
                 return false;
             }
-           else {
-                    System.out.println("Pasa en ficha");
-                }
-            if (board[row][col]!=null) {
-                tileCount.put(tile, tileCount.get(tile) - 1);
+            else {
+                System.out.println("Pasa en ficha");
+                //tileCount.put(tile, tileCount.get(tile) - 1);
+            }
+            if (board[crow][ccol] == null) {
+               tileCount.put(tile, tileCount.get(tile) - 1);
+            }
+            else{
+                enlazado=true;
             }
         }
-
-        return true;
+        if (tableroVacio) {
+            return true;
+        }
+        else {
+            return enlazado;
+        }
     }
 
-    public void actualizarTablero(String word, int fila, int columna, boolean horizontal, Character[][] tablero, FichasJugador fichasJugador,ArrayList<Character> fichasUsadas) {
+    public void actualizarTablero(String word, int fila, int columna, boolean horizontal, Character[][] tablero, FichasJugador fichasJugador, ArrayList<Character> fichasUsadas) {
         if (horizontal) {
             for (int i = 0; i < word.length(); ) {
-                if(tablero[fila][columna]==null) {
+                if (tablero[fila][columna] == null) {
                     String tile;
                     if (i < word.length() - 1 && (word.charAt(i) == 'L' && word.charAt(i + 1) == 'L')) {
                         tile = "LL";
@@ -112,13 +128,19 @@ public class ColocadorDePalabra {
                     }
                 }
                 else {
-                    i++;
+                    String simbolo = tablero[fila][columna].getSymbol();
+                    if (simbolo.equals("CH") || simbolo.equals("LL") || simbolo.equals("RR")) {
+                        i=i+2;
+                    }
+                    else {
+                        i=i+1;
+                    }
                     columna++;
                 }
             }
         } else {
             for (int i = 0; i < word.length(); ) {
-                if(tablero[fila][columna]==null) {
+                if (tablero[fila][columna] == null) {
                     String tile;
                     if (i < word.length() - 1 && (word.charAt(i) == 'L' && word.charAt(i + 1) == 'L')) {
                         tile = "LL";
@@ -155,7 +177,13 @@ public class ColocadorDePalabra {
                     }
                 }
                 else {
-                    i++;
+                    String simbolo = tablero[fila][columna].getSymbol();
+                    if (simbolo.equals("CH") || simbolo.equals("LL") || simbolo.equals("RR")) {
+                        i=i+2;
+                    }
+                    else {
+                        i=i+1;
+                    }
                     fila++;
                 }
             }
