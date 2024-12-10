e995da4943555a3467ab3476b6ac5fbc92c3b492
josegg29 <jose.gregorio.jr.29@gmail.com>
2024-12-09 22:34:47 -0400

    comentarios JavaDoc

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,35 @@
 import java.util.Scanner;
 
 /**
- * Clase principal que inicia y gestiona el flujo del juego, permitiendo a los jugadores iniciar sesión,
- * registrarse, continuar partidas anteriores y ver estadísticas.
+ * Clase principal que inicia y gestiona el flujo del juego, permitiendo a los jugadores
+ * iniciar sesión, registrarse, continuar partidas anteriores y ver estadísticas.
  * También permite a los jugadores iniciar nuevas partidas o continuar partidas guardadas.
  */
 public class Main {
 
+    /**
+     * Manejador de archivos para gestionar operaciones relacionadas con el almacenamiento y recuperación de datos.
+     */
     public static ManejadorDeArchivos archivos = new ManejadorDeArchivos();
+
+    /**
+     * Scanner para leer entradas del usuario desde la consola.
+     */
     public static Scanner read = new Scanner(System.in);
+
+    /**
+     * Bandera para validar la entrada de números por el usuario.
+     */
     public static boolean validInput = false;
 
+    // Constantes de estilo de texto y color para la consola
     public static String RESET = "\u001B[0m";
     public static String TEXTO_ROJO = "\u001B[31m";
     public static String TEXTO_VERDE = "\u001B[32m";
     public static String TEXTO_AZUL = "\u001B[34m";
     public static String TEXTO_NEGRO = "\u001B[30m";
 
-    public static String FONDO_BLANCO= "\u001B[107m";
+    public static String FONDO_BLANCO = "\u001B[107m";
     public static String FONDO_VERDE = "\u001B[102m";
     public static String FONDO_AMARILLO = "\u001B[103m";
     public static String FONDO_CYAN = "\u001B[106m";
@@ -26,25 +38,27 @@ public class Main {
      * Inicia el proceso de inicio de sesión o registro de un jugador.
      * Permite al jugador iniciar sesión si ya tiene una cuenta, o registrarse si es un nuevo jugador.
      *
-     * @return Un objeto Jugador con la información del jugador autenticado o registrado.
+     * @return Un objeto {@link Jugador} con la información del jugador autenticado o registrado.
      */
     public static Jugador inicio() {
         String alias = "";
         String email = "";
         Authentication auth = new Authentication();
         System.out.println(TEXTO_AZUL + "\n Ingrese la opcion que quiere elegir: " + RESET);
-        System.out.println(TEXTO_AZUL + " 1.Iniciar sesion \n 2.Registrarse \n\n 0. salir" + RESET);
+        System.out.println(TEXTO_AZUL + " 1. Iniciar sesión \n 2. Registrarse \n\n 0. Salir" + RESET);
         int opc = read.nextInt();
 
         while (opc != 0) {
             if ((opc == 1) || (opc == 2)) {
-                System.out.println("Ingresa el correo electronico");
+                System.out.println("Ingresa el correo electrónico");
                 email = read.next();
+
                 // Validar el correo electrónico
                 while (!auth.validateEmail(email)) {
-                    System.out.println(TEXTO_ROJO + "Correo invalido, por favor ingrese otro" + RESET);
+                    System.out.println(TEXTO_ROJO + "Correo inválido, por favor ingrese otro" + RESET);
                     email = read.next();
                 }
+
                 System.out.println("Ingresa el alias del jugador");
                 alias = read.next();
 
@@ -52,23 +66,28 @@ public class Main {
                 if (opc == 1) {
                     Jugador jugador = auth.login(alias, email);
                     while (jugador == null) {
-                        System.out.print("Usuario no existe, ingrese un usuario valido");
+                        System.out.print("Usuario no existe, ingrese un usuario válido");
                         jugador = inicio();  // Recursión para intentar nuevamente
                     }
                     jugador.limpiarFichas(); // Limpiar fichas del jugador al iniciar
                     return jugador;
                 }
-                return auth.register(alias,email);
+                return auth.register(alias, email);
             } else {
-                System.out.println("Ingrese una opcion valida");
-                System.out.println("\n\n 1.Iniciar sesion \n 2.Registrarse \n\n 0. salir");
+                System.out.println("Ingrese una opción válida");
+                System.out.println("\n\n 1. Iniciar sesión \n 2. Registrarse \n\n 0. Salir");
                 opc = read.nextInt();
             }
         }
         return null;
     }
 
-    public static int leerNumero(){
+    /**
+     * Lee un número entero ingresado por el usuario, validando la entrada.
+     *
+     * @return El número leído.
+     */
+    public static int leerNumero() {
         int opc = -1;
         while (!validInput) {
             try {
@@ -81,41 +100,44 @@ public class Main {
         }
         validInput = false;
         return opc;
-    };
-    public static void menu(){
-        System.out.println("\n"+FONDO_CYAN+ TEXTO_NEGRO+"¿Que les gustaria hacer?"+ RESET);
-        System.out.println(FONDO_CYAN+ TEXTO_NEGRO+"1. Iniciar una partida"+ RESET);
-        System.out.println(FONDO_CYAN+ TEXTO_NEGRO+"2. Continuar partida anterior"+ RESET);
-        System.out.println(FONDO_CYAN+ TEXTO_NEGRO+"3. Ver estadisticas de los jugadores"+ RESET);
-        System.out.println(FONDO_CYAN+ TEXTO_NEGRO+"0. Salir"+ RESET);
     }
 
+    /**
+     * Muestra el menú principal del juego con las opciones disponibles para los jugadores.
+     */
+    public static void menu() {
+        System.out.println("\n" + FONDO_CYAN + TEXTO_NEGRO + "¿Qué les gustaría hacer?" + RESET);
+        System.out.println(FONDO_CYAN + TEXTO_NEGRO + "1. Iniciar una partida" + RESET);
+        System.out.println(FONDO_CYAN + TEXTO_NEGRO + "2. Continuar partida anterior" + RESET);
+        System.out.println(FONDO_CYAN + TEXTO_NEGRO + "3. Ver estadísticas de los jugadores" + RESET);
+        System.out.println(FONDO_CYAN + TEXTO_NEGRO + "0. Salir" + RESET);
+    }
 
     /**
      * Método principal que gestiona el flujo del juego.
-     * Permite a dos jugadores iniciar sesión, registrarse, ver estadísticas, o continuar una partida.
-     * También permite iniciar una nueva partida o continuar una guardada.
+     * Permite a dos jugadores iniciar sesión, registrarse, ver estadísticas o continuar una partida.
+     * También inicia una nueva partida o carga una guardada.
      *
      * @param args Argumentos de línea de comandos (no utilizados en este caso).
      */
     public static void main(String[] args) {
-        System.out.println(FONDO_VERDE+TEXTO_NEGRO + "****************************************" +RESET);
-        System.out.println(FONDO_VERDE+TEXTO_NEGRO + "***********Inicio del juego*************" + RESET);
-        System.out.println(FONDO_VERDE+TEXTO_NEGRO + "***************Jugador 1****************"+ RESET);
+        System.out.println(FONDO_VERDE + TEXTO_NEGRO + "****************************************" + RESET);
+        System.out.println(FONDO_VERDE + TEXTO_NEGRO + "*********** Inicio del juego *************" + RESET);
+        System.out.println(FONDO_VERDE + TEXTO_NEGRO + "*************** Jugador 1 ****************" + RESET);
         Jugador jugador1 = inicio();
-        System.out.println(FONDO_VERDE+TEXTO_NEGRO + "***************Jugador 2****************"+ RESET);
+        System.out.println(FONDO_VERDE + TEXTO_NEGRO + "*************** Jugador 2 ****************" + RESET);
         Jugador jugador2 = inicio();
-            System.out.println("\n"+FONDO_CYAN+ TEXTO_NEGRO+"¿Que les gustaria hacer?"+ RESET);
-            menu();
+
+        menu();
         int opc = leerNumero();
-        while(opc != 0){
-            if(opc == 1){
-                Partida partida = new Partida(jugador1,jugador2);
+        while (opc != 0) {
+            if (opc == 1) {
+                Partida partida = new Partida(jugador1, jugador2);
                 partida.game();
                 menu();
                 opc = leerNumero();
-            }else if (opc == 2){
-                if(jugador1!=null && jugador2 != null){
+            } else if (opc == 2) {
+                if (jugador1 != null && jugador2 != null) {
                     Partida continuar = archivos.buscarPartida(jugador1.getAlias(), jugador2.getAlias());
                     if (continuar == null) {
                         System.out.println("No hay ninguna partida guardada para cargar.");
@@ -123,26 +145,23 @@ public class Main {
                         continuar.continuarPartida();
                     }
                 }
-                System.out.println(RESET+"\n"+FONDO_CYAN+ TEXTO_NEGRO+"¿Que les gustaria hacer?"+ RESET);
                 menu();
                 opc = leerNumero();
-            }else if (opc == 3) {
+            } else if (opc == 3) {
                 Estadisticas estadisticas = new Estadisticas();
-                if ((jugador1 != null) && (jugador2 != null)){
-                System.out.println(FONDO_VERDE+TEXTO_NEGRO + "***************Jugador 1****************");
-                estadisticas.estad(jugador1.getAlias());
-                System.out.println(FONDO_AMARILLO+TEXTO_NEGRO + "***************Jugador 2****************");
-                estadisticas.estad(jugador2.getAlias());
+                if ((jugador1 != null) && (jugador2 != null)) {
+                    System.out.println(FONDO_VERDE + TEXTO_NEGRO + "*************** Jugador 1 ****************");
+                    estadisticas.estad(jugador1.getAlias());
+                    System.out.println(FONDO_AMARILLO + TEXTO_NEGRO + "*************** Jugador 2 ****************");
+                    estadisticas.estad(jugador2.getAlias());
                 }
-                System.out.println(RESET+"\n"+FONDO_CYAN+ TEXTO_NEGRO+"¿Que les gustaria hacer?"+ RESET);
                 menu();
                 opc = leerNumero();
             } else {
-                System.out.println("Ingrese una opcion valida");
+                System.out.println("Ingrese una opción válida");
                 menu();
                 opc = leerNumero();
             }
         }
-
     }
 }
--- src/MainUsuarios.java
+++ src/MainUsuarios.java
@@ -3,7 +3,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-
+/**
+ * Clase principal para la gestión de usuarios en el sistema del juego.
+ * Proporciona funcionalidades como mostrar usuarios, registrar nuevos,
+ * modificar datos y eliminar usuarios existentes.
+ */
 public class MainUsuarios {
     public static Scanner read = new Scanner(System.in);
     public static boolean validInput = false;
@@ -19,11 +23,14 @@ public class MainUsuarios {
     public static String FONDO_AMARILLO = "\u001B[103m";
     public static String FONDO_CYAN = "\u001B[106m";
 
-
-    public static void mostrarUsuarios(){
+    /**
+     * Muestra una lista de todos los usuarios registrados en el sistema.
+     * Los usuarios se recuperan desde archivos almacenados localmente.
+     */
+    public static void mostrarUsuarios() {
         ManejadorDeArchivos manejadorDeArchivos = new ManejadorDeArchivos();
         File directory = new File(System.getProperty("user.dir"));
-        String extension = ".jug";  // Extensión de los archivos de partida
+        String extension = ".jug";  // Extensión de los archivos de usuario
         List<String> fileNames = BuscarArchivo.getFileNamesWithExtension(directory, extension);
 
         ArrayList<Jugador> jugadores = new ArrayList<>();
@@ -34,25 +41,29 @@ public class MainUsuarios {
             jugadores.add(jugador);
         }
 
-        // Muestra los encabezados de la tabla de estadísticas
+        // Mostrar encabezados de la tabla de usuarios
         System.out.print("alias");
         System.out.printf("%15s ", "correo");
         System.out.println("");
 
         for (Jugador jugador : jugadores) {
             System.out.println(jugador.getAlias());
-            System.out.printf("%15s",jugador.getEmail());
+            System.out.printf("%15s", jugador.getEmail());
             System.out.println("");
         }
     }
 
+    /**
+     * Permite modificar los datos de un usuario existente.
+     * El usuario se busca por su alias, y se pueden actualizar el alias y/o el correo electrónico.
+     */
     public static void modificarUsuario() {
         ManejadorDeArchivos manejadorDeArchivos = new ManejadorDeArchivos();
         System.out.println("Ingrese el alias del usuario a modificar:");
         String alias = read.next();
 
         File directory = new File(System.getProperty("user.dir"));
-        String extension = ".jug";  // Extensión de los archivos de partida
+        String extension = ".jug";  // Extensión de los archivos de usuario
         List<String> fileNames = BuscarArchivo.getFileNamesWithExtension(directory, extension);
 
         boolean usuarioEncontrado = false;
@@ -102,57 +113,73 @@ public class MainUsuarios {
         System.out.println(TEXTO_VERDE + "Usuario modificado exitosamente." + RESET);
     }
 
-
-    public static void registrarUsuario(){
+    /**
+     * Registra un nuevo usuario solicitando el alias y correo electrónico.
+     * Realiza una validación básica del correo antes de guardar los datos.
+     */
+    public static void registrarUsuario() {
         String alias = "";
         String email = "";
         Authentication auth = new Authentication();
-        System.out.println(TEXTO_AZUL + "\n Ingrese la opcion que quiere elegir: " + RESET);
+        System.out.println(TEXTO_AZUL + "\nIngrese la opción que quiere elegir: " + RESET);
         int opc = read.nextInt();
-        System.out.println("Ingresa el correo electronico");
+        System.out.println("Ingresa el correo electrónico");
         email = read.next();
         // Validar el correo electrónico
         while (!auth.validateEmail(email)) {
-            System.out.println(TEXTO_ROJO + "Correo invalido, por favor ingrese otro" + RESET);
+            System.out.println(TEXTO_ROJO + "Correo inválido, por favor ingrese otro" + RESET);
             email = read.next();
         }
         System.out.println("Ingresa el alias del jugador");
         alias = read.next();
-        auth.register(alias,email);
+        auth.register(alias, email);
         System.out.println("El usuario fue registrado");
     }
 
-    public static void eliminarUsuario(String alias){
+    /**
+     * Elimina un usuario del sistema basado en su alias.
+     *
+     * @param alias El alias del usuario a eliminar.
+     */
+    public static void eliminarUsuario(String alias) {
         ManejadorDeArchivos manejadorDeArchivos = new ManejadorDeArchivos();
         File directory = new File(System.getProperty("user.dir"));
-        String extension = ".jug";  // Extensión de los archivos de partida
+        String extension = ".jug";  // Extensión de los archivos de usuario
         List<String> fileNames = BuscarArchivo.getFileNamesWithExtension(directory, extension);
 
-        ArrayList<Jugador> jugadores = new ArrayList<>();
         for (String fileName : fileNames) {
             int lastIndex = fileName.lastIndexOf(".");
             String jugador = fileName.substring(0, lastIndex);
-            if (alias.equals(jugador)){
+            if (alias.equals(jugador)) {
                 File archivo = new File(fileName);
-                if (archivo.delete()){
-                    System.out.println("el usuario fue eliminado");
+                if (archivo.delete()) {
+                    System.out.println("El usuario fue eliminado");
                 } else {
-                    System.out.println("el usuario no se puedo eliminar");
+                    System.out.println("El usuario no se pudo eliminar");
                 }
             }
         }
     }
 
-    public static void menu(){
-        System.out.println("\n"+FONDO_CYAN+ TEXTO_NEGRO+"¿Que le gustaria hacer?"+ RESET);
-        System.out.println(FONDO_CYAN+ TEXTO_NEGRO+"1. ver todos los usuarios"+ RESET);
-        System.out.println(FONDO_CYAN+ TEXTO_NEGRO+"2. registrar usuario"+ RESET);
-        System.out.println(FONDO_CYAN+ TEXTO_NEGRO+"3. eliminar usuario"+ RESET);
-        System.out.println(FONDO_CYAN+ TEXTO_NEGRO+"4. modificar usuario"+ RESET);
-        System.out.println(FONDO_CYAN+ TEXTO_NEGRO+"0. Salir"+ RESET);
+    /**
+     * Muestra el menú principal para la gestión de usuarios.
+     * Permite al usuario seleccionar diferentes opciones de gestión.
+     */
+    public static void menu() {
+        System.out.println("\n" + FONDO_CYAN + TEXTO_NEGRO + "¿Qué le gustaría hacer?" + RESET);
+        System.out.println(FONDO_CYAN + TEXTO_NEGRO + "1. Ver todos los usuarios" + RESET);
+        System.out.println(FONDO_CYAN + TEXTO_NEGRO + "2. Registrar usuario" + RESET);
+        System.out.println(FONDO_CYAN + TEXTO_NEGRO + "3. Eliminar usuario" + RESET);
+        System.out.println(FONDO_CYAN + TEXTO_NEGRO + "4. Modificar usuario" + RESET);
+        System.out.println(FONDO_CYAN + TEXTO_NEGRO + "0. Salir" + RESET);
     }
 
-    public static int leerNumero(){
+    /**
+     * Lee un número ingresado por el usuario y lo valida.
+     *
+     * @return Un número entero ingresado por el usuario.
+     */
+    public static int leerNumero() {
         int opc = -1;
         while (!validInput) {
             try {
@@ -165,37 +192,41 @@ public class MainUsuarios {
         }
         validInput = false;
         return opc;
-    };
+    }
 
+    /**
+     * Método principal que gestiona el flujo de la aplicación.
+     * Presenta un menú de opciones y ejecuta las acciones correspondientes.
+     *
+     * @param args Argumentos de línea de comandos (no utilizados en este caso).
+     */
     public static void main(String[] args) {
-        System.out.println(FONDO_VERDE+TEXTO_NEGRO + "****************************************" +RESET);
-        System.out.println(FONDO_VERDE+TEXTO_NEGRO + "***********Gestion de Usuarios*************" + RESET);
+        System.out.println(FONDO_VERDE + TEXTO_NEGRO + "****************************************" + RESET);
+        System.out.println(FONDO_VERDE + TEXTO_NEGRO + "*********** Gestión de Usuarios *************" + RESET);
         menu();
         int opc = leerNumero();
-        while(opc != 0){
-            if(opc == 1){
-                mostrarUsuarios();
-                menu();
-                opc = leerNumero();
-            }else if (opc == 2){
-                registrarUsuario();
-                menu();
-                opc = leerNumero();
-            }else if (opc == 3) {
-                System.out.println("Ingrese el alias del usuario a eliminar");
-                String alias=read.nextLine();
-                eliminarUsuario(alias);
-                menu();
-                opc = leerNumero();
-            }else if (opc == 4) {
-                modificarUsuario();
-                menu();
-                opc = leerNumero();
-            } else {
-                System.out.println("Ingrese una opcion valida");
-                menu();
-                opc = leerNumero();
+        while (opc != 0) {
+            switch (opc) {
+                case 1:
+                    mostrarUsuarios();
+                    break;
+                case 2:
+                    registrarUsuario();
+                    break;
+                case 3:
+                    System.out.println("Ingrese el alias del usuario a eliminar");
+                    String alias = read.nextLine();
+                    eliminarUsuario(alias);
+                    break;
+                case 4:
+                    modificarUsuario();
+                    break;
+                default:
+                    System.out.println("Ingrese una opción válida");
+                    break;
             }
+            menu();
+            opc = leerNumero();
         }
     }
 }
