10aba98a384b2a48be0308f14069504f9232d26e
josegg29 <jose.gregorio.jr.29@gmail.com>
2025-01-16 19:21:17 -0400

    onButtonEstadisticas

--- src/usuarios/com/example/usuarios/MenuDePartidaController.java
+++ src/usuarios/com/example/usuarios/MenuDePartidaController.java
@@ -1,7 +1,12 @@
 package com.example.usuarios;
 
 import javafx.fxml.FXML;
-import juego.Jugador;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 public class MenuDePartidaController {
     private Jugador jugador1;
@@ -17,8 +22,26 @@ public class MenuDePartidaController {
     @FXML
     protected void onButtonContinuarJuego(){}
     @FXML
-    protected void onButtonEstadisticas(){}
-    @FXML
     protected void onButtonRegresar(){}
 
+    @FXML
+    private Button buttonEstadisticas;
+
+    @FXML
+    protected void onButtonEstadisticas() {
+
+        try {
+            Stage currentStage = (Stage) buttonEstadisticas.getScene().getWindow();
+            currentStage.close();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/resources/MenuDePartida/MenuDeEstadisticas.fxml"));
+            Parent root = loader.load();
+
+            Stage newStage = new Stage();
+            newStage.setTitle("Estadísticas");
+            newStage.setScene(new Scene(root));
+            newStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
