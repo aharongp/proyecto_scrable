b53884000de4de737ce705dd72d6787f4666dd68
aharonguedez <aharon.guedez23@gmail.com>
2025-01-17 01:46:44 -0400

    tablero

--- src/usuarios/com/example/usuarios/TableroController.java
+++ src/usuarios/com/example/usuarios/TableroController.java
@@ -98,11 +98,7 @@ public class TableroController {
             }
             sendButton.setOnMouseClicked(event -> onSendClick());
             passButton.setOnMouseClicked(event -> onPassClick());
-            if (partida.getActualTurn() ==1){
-                statsButton.setOnMouseClicked(event -> showPlayerStats(partida.getJugador1()));
-            }else {
-                statsButton.setOnMouseClicked(event -> showPlayerStats(partida.getJugador2()));
-            }
+            statsButton.setOnMouseClicked(event -> showPlayerStats());
         }
 
         // Asignar evento a los botones del tablero
@@ -317,7 +313,13 @@ public class TableroController {
         alert.setContentText(message);
         alert.showAndWait();
     }
-    public void showPlayerStats(Jugador jugador) {
+    public void showPlayerStats() {
+        Jugador jugador;
+        if (partida.getActualTurn() == 1){
+            jugador = partida.getJugador1();
+        }else {
+            jugador = partida.getJugador2();
+        }
         // Crear una nueva alerta
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Estadísticas del Jugador");
